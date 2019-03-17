01d3a5e1abba76891833557c493c00135b9ce9fe
huxiaoheng44 <44961581+huxiaoheng44@users.noreply.github.com>
2019-03-17 21:29:57 +0800

    黄俊杰需要的

--- app/src/main/java/com/example/dress/activity/StampViewActivity.java
+++ app/src/main/java/com/example/dress/activity/StampViewActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -18,16 +19,21 @@ import java.util.List;
 
 public class StampViewActivity extends AppCompatActivity {
 
+    int[][] stampList=new int[11][11];              //邮票信息的二维数组
+
     List<Stamp> stamps = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Intent intent = getIntent();
+        int tag=intent.getIntExtra("tag",0);
+
         setContentView(R.layout.activity_stamp_view);
         RecyclerView recyclerView = (RecyclerView)findViewById(R.id.stamp_recycleView);
         GridLayoutManager gridLayoutManager = new GridLayoutManager(this,2);
         recyclerView.setLayoutManager(gridLayoutManager);
         //设置内容
-        initStampList();
+        initStampList_1();
         StampAdapter stampAdapter = new StampAdapter(stamps);
         recyclerView.setAdapter(stampAdapter);
 
@@ -37,13 +43,22 @@ public class StampViewActivity extends AppCompatActivity {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
     }
-    private void initStampList(){
+    private void initStampList_1(){
         Stamp temp1 = new Stamp(R.drawable.text_notyet,1,0,1);
         Stamp temp2 = new Stamp(R.drawable.text_notyet,1,0,1);
         Stamp temp3 = new Stamp(R.drawable.text_notyet,1,0,1);
         temp1.setText("0/9");
         temp2.setText("0/9");
         stamps.add(temp1);stamps.add(temp2);stamps.add(temp3);
+    }
+
+    private void initStampList_2(){
+
+    }
+
+    //获取邮票二维数组的信息
+    private void getStampMecessage(){
+
     }
 
     @Override
